9019d1e915cb655e46af6e27a362d2ac36b69e4d
Shrijan Maharjan <shrijanm29@gmail.com>
2025-08-20 20:08:52 +0545

    feat: Added register feature

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -2,9 +2,25 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\User;
 use Illuminate\Http\Request;
+
+use function Laravel\Prompts\password;
 
 class UserController extends Controller
 {
     //
+    public function register(Request $request)
+    {
+        $validate = $request->validate([
+            'name' => 'required|string',
+            'email' => 'required|unique:users|string|email',
+            'password' => 'required|min:8|confirmed'
+        ]);
+        User::create($validate);
+        return response()->json([
+            'status' => 'true',
+            'message' => 'User registered successfully'
+        ]);
+    }
 }
